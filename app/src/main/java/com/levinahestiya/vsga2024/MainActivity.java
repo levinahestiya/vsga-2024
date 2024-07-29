45e8bc6134c7b6b5574ac1b36589dc43a2b55d4f
Levina Hestiya Aryani <levinahestiya@gmail.com>
2024-07-29 08:33:16 +0700

    Membuat fitur baca dan hapus file

--- app/src/main/java/com/levinahestiya/vsga2024/MainActivity.java
+++ app/src/main/java/com/levinahestiya/vsga2024/MainActivity.java
@@ -10,8 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         editText = findViewById(R.id.editText);
+
+        bacaFile();
     }
 
     private void buatFile() {
@@ -46,8 +51,42 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void bacaFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()) {
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null) {
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            editText.setText(text);
+        }
+        else {
+            editText.setText("");
+        }
+    }
+
+    private void hapusFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()) {
+            file.delete();
+            editText.setText("");
+        }
+    }
+
     public void onClick(View view) {
         if (view.getId() == R.id.button1)
             buatFile();
+        else if (view.getId() == R.id.button2)
+            bacaFile();
+        else if (view.getId() == R.id.button4)
+            hapusFile();
     }
 }
