0a141f407584099edb2263749aab2bad707b390d
Levina Hestiya Aryani <levinahestiya@gmail.com>
2024-07-29 08:28:32 +0700

    Membuat fitur simpan ke file

--- app/src/main/java/com/levinahestiya/vsga2024/MainActivity.java
+++ app/src/main/java/com/levinahestiya/vsga2024/MainActivity.java
@@ -1,6 +1,8 @@
 package com.levinahestiya.vsga2024;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FILENAME = "catatan.txt";
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editText = findViewById(R.id.editText);
+    }
+
+    private void buatFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        FileOutputStream fos;
+        try {
+            file.createNewFile();
+            fos = new FileOutputStream(file, false);
+            fos.write(editText.getText().toString().getBytes());
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.button1)
+            buatFile();
     }
 }
