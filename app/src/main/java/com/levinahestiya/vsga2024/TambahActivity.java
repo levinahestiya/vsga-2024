d93bf5253f395601e47beea8b6beca2df1f98163
Levina Hestiya Aryani <levinahestiya@gmail.com>
2024-07-31 09:58:40 +0700

    Membuat layout utama dan form tambah kontak

--- app/src/main/java/com/levinahestiya/vsga2024/TambahActivity.java
+++ app/src/main/java/com/levinahestiya/vsga2024/TambahActivity.java
@@ -1,6 +1,5 @@
 package com.levinahestiya.vsga2024;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-public class MainActivity extends AppCompatActivity {
-
-    private FloatingActionButton fab;
+public class TambahActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tambah);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        fab = findViewById(R.id.floatingActionButton);
-        fab.setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity.this, TambahActivity.class);
-            startActivity(i);
         });
     }
 }
